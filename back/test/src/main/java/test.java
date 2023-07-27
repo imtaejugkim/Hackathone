d1a6d0a9c82d60523fd8ad08c8e34ba5217bbce5
bangbang444 <140622878+bangbang444@users.noreply.github.com>
2023-07-27 18:00:10 +0900

    test

--- back/test/src/main/java/test.java
+++ back/test/src/main/java/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("test44444444");
+        for(int i=0; i<10; i++)
+            System.out.println("*********");
     }
 }
