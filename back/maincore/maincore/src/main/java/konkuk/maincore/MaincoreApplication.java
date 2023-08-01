c43da267d18e9d4e37916fa653e96e90f400f8d4
bangbang444 <140622878+bangbang444@users.noreply.github.com>
2023-08-01 22:12:11 +0900

    join

---
+++ back/maincore/maincore/src/main/java/konkuk/maincore/MaincoreApplication.java
@@ -0,0 +1,14 @@
+package konkuk.maincore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaincoreApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MaincoreApplication.class, args);
+	}
+
+}
