a8f84fe2453d101916c8aa5d564ac10fd84ba99a
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2023-08-02 12:47:13 +0900

    사진첩 불러오기 임의

---
+++ front/HackathoneOneBite/app/src/main/java/com/example/hackathoneonebite/main/fragment/AdapterMain3Posting.kt
@@ -0,0 +1,35 @@
+package com.example.hackathoneonebite.main.fragment
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.example.hackathoneonebite.R
+
+class AdapterMain3Posting(private val photoList: List<String>) : RecyclerView.Adapter<AdapterMain3Posting.PhotoViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_photo, parent, false)
+        return PhotoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        val photoPath = photoList[position]
+        val options = RequestOptions()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+        Glide.with(holder.itemView.context)
+            .load(photoPath)
+            .apply(options)
+            .into(holder.photoImageView)
+    }
+
+    override fun getItemCount(): Int = photoList.size
+
+    class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val photoImageView: ImageView = itemView.findViewById(R.id.photoImageView)
+    }
+}
--- front/HackathoneOneBite/app/src/main/java/com/example/hackathoneonebite/main/fragment/Main3PostingFragment.kt
+++ front/HackathoneOneBite/app/src/main/java/com/example/hackathoneonebite/main/fragment/Main3PostingFragment.kt
@@ -1,60 +1,96 @@
 package com.example.hackathoneonebite.main.fragment
 
+import android.Manifest
+import android.content.ContentUris
+import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.hackathoneonebite.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Main3PostingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Main3PostingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    companion object {
+        private const val REQUEST_READ_EXTERNAL_STORAGE = 1
+    }
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: AdapterMain3Posting
+    private val photoList = mutableListOf<String>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_main3_posting, container, false)
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = GridLayoutManager(context, 3)
+        adapter = AdapterMain3Posting(photoList)
+        recyclerView.adapter = adapter
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            loadPhotos()
+        } else {
+            requestExternalStoragePermission()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main3_posting, container, false)
+    private fun requestExternalStoragePermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            REQUEST_READ_EXTERNAL_STORAGE
+        )
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainPostingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Main3PostingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun loadPhotos() {
+        val projection = arrayOf(MediaStore.Images.Media._ID)
+        val cursor: Cursor? = requireActivity().contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            null,
+            null,
+            null
+        )
+
+        cursor?.use {
+            val idColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+            while (it.moveToNext()) {
+                val imageId = it.getLong(idColumn)
+                val imagePath = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    imageId
+                ).toString()
+                photoList.add(imagePath)
             }
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_READ_EXTERNAL_STORAGE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                loadPhotos()
+            } else {
+                // 권한이 거부된 경우 처리
+            }
+        }
     }
 }
