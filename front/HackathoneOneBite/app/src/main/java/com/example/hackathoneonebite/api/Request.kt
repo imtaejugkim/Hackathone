a2eea6e4535c21d1ef51ce9f5c8acbcefa134cd4
SungWoon Kim <slm0523@naver.com>
2023-08-09 14:46:43 +0900

    rr

--- front/HackathoneOneBite/app/src/main/java/com/example/hackathoneonebite/api/Request.kt
+++ front/HackathoneOneBite/app/src/main/java/com/example/hackathoneonebite/api/Request.kt
@@ -7,3 +7,11 @@ data class Main1LoadPostRequest(
     @SerializedName("theme")var theme: String = "0",
     @SerializedName("image")var image: MultipartBody.Part? = null
 )
+
+data class Main3UploadPostRequest(
+    @SerializedName("imgArray")var imgArray: ArrayList<MultipartBody.Part> =ArrayList(),
+    @SerializedName("theme")var theme: Int = 0,
+    @SerializedName("user_id")var user_id: String = "",
+    @SerializedName("music_num")var music_num: String = "",
+    @SerializedName("text")var message: String = ""
+)
