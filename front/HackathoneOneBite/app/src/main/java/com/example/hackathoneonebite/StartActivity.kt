411836e8de3fd9c53b1f326b63fe8191206a519f
TaejungKim <tae0803@naver.com>
2023-08-10 00:08:38 +0900

    google 완료
    google완료

--- front/HackathoneOneBite/app/src/main/java/com/example/hackathoneonebite/StartActivity.kt
+++ front/HackathoneOneBite/app/src/main/java/com/example/hackathoneonebite/StartActivity.kt
@@ -2,6 +2,7 @@ package com.example.hackathoneonebite
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.ContentValues
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.IntentSender
@@ -14,18 +15,27 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat.startActivity
+import com.bumptech.glide.Glide.init
 import com.example.hackathoneonebite.Data.Post
 import com.example.hackathoneonebite.Data.User
 import com.example.hackathoneonebite.api.Main1LoadPostRequest
 import com.example.hackathoneonebite.api.Main1LoadPostResponse
 import com.example.hackathoneonebite.api.RetrofitBuilder
 import com.example.hackathoneonebite.databinding.ActivityStartBinding
+import com.example.hackathoneonebite.main.MainFrameActivity
+import com.example.hackathoneonebite.main.fragment.Main1HomeFirstFragment
+import com.example.hackathoneonebite.main.fragment.Main3PostingTimeActivity
+import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -34,24 +44,145 @@ import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.Exception
 
 
 class StartActivity : ComponentActivity() {
+    var auth:FirebaseAuth?= null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
     lateinit var mGoogleSignInClient: GoogleSignInClient
     lateinit var resultLauncher: ActivityResultLauncher<Intent>
     lateinit var binding: ActivityStartBinding
+
+    private lateinit var btnGoogle:Button
+    private lateinit var btnEmail:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setResultSignUp()
-        setGoogleLogin()
-        init()
+        //setResultSignUp()
+        //init()
+
+        btnEmail = binding.btmEmail
+        btnGoogle = binding.btnGoogle
+
+        auth = FirebaseAuth.getInstance()
+        btnEmail.setOnClickListener {
+            val strEmail = binding.userID.toString()
+            val strPwd = binding.userPW.toString()
+            try{
+                signAndSignUp(strEmail, strPwd)
+            } catch (e:java.lang.Exception) {
+                Toast.makeText(this, "아이디와 비밀번호를 제대로 입력하세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnGoogle.setOnClickListener {
+            googleLogin()
+        }
+
+        binding.loginBtn.setOnClickListener {
+            setBasicLogin()
+        }
     }
 
-    override fun onStart() {
+    private fun googleLogin(){
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        val signInIntent = googleSignInClient.signInIntent
+        signInLauncher.launch(signInIntent)
+    }
+
+    private val signInLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val intent = result.data
+                val task = GoogleSignIn.getSignedInAccountFromIntent(intent)
+                try {
+                    val account = task.getResult(ApiException::class.java)
+                    firebaseAuthWithGoogle(account.idToken!!)
+                    Log.e("TAG", "아이디 토큰 잘 넘어감")
+
+                } catch (e: ApiException) {
+                    Log.e("TAG", "Google sign-in failed", e)
+                }
+            }
+        }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth!!.currentUser
+                    Log.d("TAG", "Signed in with Google: ${user?.displayName}")
+                    movePage(task.result?.user)
+
+                    val account = GoogleSignIn.getLastSignedInAccount(this)
+                    val email = account?.email
+                    val familyName = account?.familyName
+                    val givenName = account?.givenName
+                    val displayName = account?.displayName
+                    val photoUrl = account?.photoUrl
+
+                    // Now you have access to the user's Gmail information
+                    Log.d("TAG", "Email: $email")
+                    Log.d("TAG", "Family Name: $familyName")
+                    Log.d("TAG", "Given Name: $givenName")
+                    Log.d("TAG", "Display Name: $displayName")
+                    Log.d("TAG", "Photo URL: $photoUrl")
+                } else {
+                    Log.e("TAG", "Google sign-in failed", task.exception)
+                }
+            }
+    }
+
+
+    private fun signAndSignUp(email:String, pwd:String) {
+       auth?.createUserWithEmailAndPassword(email, pwd)?.addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) { // 회원가입 성공
+                movePage(task.result?.user)
+            } else { // 회원가입 실패
+                signInEmail(email, pwd)
+            }
+        }
+    }
+
+    private fun signInEmail(email:String, pwd:String) {
+        auth?.signInWithEmailAndPassword(email, pwd)?.addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) { // 로그인 성공
+                movePage(task.result?.user)
+            } else { // 로그인 실패
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+
+    private fun movePage(user:FirebaseUser?) {
+        if (user != null) {
+            Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, MainFrameActivity::class.java))
+        }
+    }
+
+
+
+    private fun setBasicLogin() {
+        val nextIntent = Intent(this, MainFrameActivity::class.java)
+        startActivity(nextIntent)
+
+    }
+
+    /*override fun onStart() {
         super.onStart()
         //로그인 되어 있는 상태면 null이 아닌것을 return
         //로그인이 안 되어 있으면 null을 return
@@ -63,13 +194,10 @@ class StartActivity : ComponentActivity() {
         }
     }
 
-    private fun setGoogleLogin() {
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .requestProfile()
-            .build()
+    private fun setGoogleLogin(auth: FirebaseAuth) {
 
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
     }
 
     private fun setResultSignUp() {
@@ -101,7 +229,7 @@ class StartActivity : ComponentActivity() {
         } catch (e: ApiException) {
             Log.w("failed", "signInResult: failed code = " + e.statusCode)
         }
-    }
+    }*/
 
     @SuppressLint("ResourceType")
     fun init() {
