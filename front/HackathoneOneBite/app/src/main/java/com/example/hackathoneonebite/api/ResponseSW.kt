e4680fff73a3f25439271ce2455ba4d0ac51c2ea
SungWoon Kim <slm0523@naver.com>
2023-08-13 00:23:33 +0900

    수정

--- front/HackathoneOneBite/app/src/main/java/com/example/hackathoneonebite/api/ResponseSW.kt
+++ front/HackathoneOneBite/app/src/main/java/com/example/hackathoneonebite/api/ResponseSW.kt
@@ -10,14 +10,14 @@ data class LoginSignInResponse(
     @SerializedName("id") val id: Long //최초 가입 시 생성된 아이디
 )
 data class LoginCheckEmailExistResponse(
-    @SerializedName("exist")val isExist: Boolean, //true이면 email이 이미 존재함.
+    @SerializedName("exists")val isExist: Boolean, //true이면 email이 이미 존재함.
     @SerializedName("id")val id: Long, //exist false인 경우에는 -1을 담아준다.
     @SerializedName("userId")val userId: String
 )
 
 //메인화면
 data class Main1LoadPostResponse(
-    @SerializedName("id") val postId: Long,
+    @SerializedName("id") val postId: ArrayList<Long>,
     @SerializedName("images") val images: ArrayList<String>,
     @SerializedName("theme") val theme: Int,
     @SerializedName("likeCount") val likeCount: Int,
