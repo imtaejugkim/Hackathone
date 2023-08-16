f0eb1372b262c99ed5fdeac985e0396711264424
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2023-08-16 17:08:24 +0900

    작은 수정

--- back/hackerthon-relay/hackerthon-relay/src/main/java/com/konkuk/hackerthonrelay/pictureupload/ImageUploadController.java
+++ back/hackerthon-relay/hackerthon-relay/src/main/java/com/konkuk/hackerthonrelay/pictureupload/ImageUploadController.java
@@ -2,28 +2,35 @@ package com.konkuk.hackerthonrelay.pictureupload;
 
 import java.io.IOException;
 import java.time.Duration;
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import com.konkuk.hackerthonrelay.notification.Notification;
-import com.konkuk.hackerthonrelay.notification.NotificationRepository;
-import com.konkuk.hackerthonrelay.pictureupload.tag.Tag;
-import com.konkuk.hackerthonrelay.pictureupload.tag.TagRepository;
-import com.konkuk.hackerthonrelay.pictureupload.tag.TagService;
-import lombok.extern.slf4j.Slf4j;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.konkuk.hackerthonrelay.comment.Comment;
-import com.konkuk.hackerthonrelay.comment.CommentRepository;
+import com.konkuk.hackerthonrelay.notification.Notification;
+import com.konkuk.hackerthonrelay.notification.Notification.NotificationType;
+import com.konkuk.hackerthonrelay.notification.NotificationRepository;
+import com.konkuk.hackerthonrelay.pictureupload.tag.Tag;
+import com.konkuk.hackerthonrelay.pictureupload.tag.TagRepository;
+import com.konkuk.hackerthonrelay.pictureupload.tag.TagService;
 import com.konkuk.hackerthonrelay.user.User;
 import com.konkuk.hackerthonrelay.user.UserRepository;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
@@ -248,7 +255,7 @@ public class ImageUploadController {
 		Notification notification = new Notification();
 		notification.setRecipient(mentioned);
 		notification.setMessage(currentUser.getUsername() + "님이 릴레이를 신청하셨습니다.");
-		notification.setType(NotificationType.COMMENT);
+		notification.setType(NotificationType.RELAY);
 		notification.setPostId(postId);
 		notification.setUserId(currentUser.getId()); // 현재 사용자의 ID
 		notification.setUserIdString(currentUser.getUserId()); // 현재 사용자의 String userId 설정
